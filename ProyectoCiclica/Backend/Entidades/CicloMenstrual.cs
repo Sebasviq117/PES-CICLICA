4d9fcf3ce99f6527ee1d9c40e8adcc777164ba63
BRV2000 <branrv2000@gmail.com>
2023-11-01 20:35:40 -0600

    Ingresar Ciclo Menstrual

---
+++ ProyectoCiclica/Backend/Entidades/CicloMenstrual.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Entidades
+{
+    public class CicloMenstrual
+    {
+       
+        public int UsuarioID { get; set; }
+        public DateTime FechaInicioCiclo { get; set; } 
+        public int DuracionCiclo { get; set;}
+        public int DuracionMenstruacion { get; set;}
+        public DateTime FechaNacimiento { get; set; }
+    }
+}
---
+++ ProyectoCiclica/Backend/Logica/LogCicloMenstrual.cs
@@ -0,0 +1,73 @@
+﻿using Backend.AccesoDatos;
+using Backend.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Logica
+{
+    public class LogCicloMenstrual
+    {
+        public ResIngresarCicloMenstrual IngresarCicloMenstrual(ReqIngresarCicloMenstrual req) { 
+            ResIngresarCicloMenstrual res  = new ResIngresarCicloMenstrual();
+
+            try 
+            {
+                if(req.elcicloMenstrual.UsuarioID == 0)
+                {
+                    res.resultado = false;
+                    res.errorMensaje = "Id Usuario Faltante";
+                }
+                else if (req.elcicloMenstrual.FechaInicioCiclo == null)
+                {
+                    res.resultado = false;
+                    res.errorMensaje = "Fecha Faltante";
+                }
+                else if (req.elcicloMenstrual.DuracionCiclo == 0 )
+                {
+                    res.resultado = false;
+                    res.errorMensaje = "Duracion Ciclo Faltante";
+                }
+                else if (req.elcicloMenstrual.DuracionMenstruacion == 0)
+                {
+                    res.resultado = false;
+                    res.errorMensaje = "Duracion Menstruacion Faltante";
+                }
+                else if (req.elcicloMenstrual.FechaNacimiento == null)
+                {
+                    res.resultado = false;
+                    res.errorMensaje = "Fecha Nacimiento faltante";
+                }
+                else
+                {
+                    //LLEGARON TODOS LOS DATOS
+                    //Enviar a base de datos
+                    int? errorId = 0;
+                    int? idReturn = 0;//idusuario
+                    string errorDescripcion = "";
+
+                    conexionlinqDataContext miLinq = new conexionlinqDataContext();
+                    miLinq.sp_IngresarRegistroCicloMenstrual(req.elcicloMenstrual.UsuarioID, req.elcicloMenstrual.FechaInicioCiclo, req.elcicloMenstrual.DuracionCiclo, req.elcicloMenstrual.DuracionMenstruacion, req.elcicloMenstrual.FechaNacimiento, ref idReturn, ref errorId, ref errorDescripcion);
+                    if (errorId == 0 && idReturn != 0)
+                    {
+                        res.resultado = true;
+                    }
+                    else { 
+                        res.resultado = false;
+                        res.errorMensaje = "error interno";
+                        Console.WriteLine(errorDescripcion);
+
+                    }
+                }
+            }    
+            catch (Exception ex) {
+                res.resultado = false;
+                res.errorMensaje = "Error interno";
+                Console.WriteLine(ex.Message);
+            }
+            return res;
+        }
+    }
+}
