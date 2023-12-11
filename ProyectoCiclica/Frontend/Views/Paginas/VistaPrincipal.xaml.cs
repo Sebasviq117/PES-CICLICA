d86c43c858c4dfde62bf8522d80d6179f175a67c
Sebasviq117 <sebasviq117@gmail.com>
2023-12-11 14:04:36 -0600

    puro Frontend

--- ProyectoCiclica/Frontend/Views/Paginas/VistaPrincipal.xaml.cs
+++ ProyectoCiclica/Frontend/Views/Paginas/VistaPrincipal.xaml.cs
@@ -1,12 +1,34 @@
+using System.Collections.ObjectModel;
 using System.Globalization;
+using System.Windows.Input;
+using Frontend.Models;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Frontend.Views;
 
 public partial class VistaPrincipal : ContentPage
 {
-	public VistaPrincipal()
-	{
-		InitializeComponent();
+    #region BindableProperty
+    public static readonly BindableProperty SelectedDateProperty = BindableProperty.Create(
+        nameof(SelectedDate),
+        typeof(DateTime),
+        declaringType: typeof(VistaPrincipal),
+        defaultBindingMode: BindingMode.TwoWay,
+        defaultValue: DateTime.Now
+        );
+    public DateTime SelectedDate
+    {
+        get => (DateTime)GetValue(SelectedDateProperty);
+        set => SetValue(SelectedDateProperty, value);
+    }
+
+    private DateTime _tempDate;
+    #endregion
+    public ObservableCollection<CalendarModel> Dates { get; set; } = new ObservableCollection<CalendarModel>();
+    public VistaPrincipal()
+    {
+        InitializeComponent();
+        BindDates(DateTime.Now);
         NavigationPage.SetHasNavigationBar(this, false);
     }
 
@@ -19,9 +41,47 @@ public partial class VistaPrincipal : ContentPage
     {
 
     }
-
-    private void CalendarView_DateSelectionChanged(object sender, EventArgs e)
+    private void BindDates(DateTime date)
     {
-        DisplayAlert("Date change", Calendar.CurrentEra.ToString(), "Ok");
+        Dates.Clear();
+        int daysCount = DateTime.DaysInMonth(date.Year, date.Month);
+
+        for (int day = 1; day <= daysCount; day++)
+        {
+            Dates.Add(new CalendarModel
+            {
+                Date = new DateTime(date.Year, date.Month, day)
+            });
+        }
+
+        var selectedDate = Dates.Where(f => f.Date.Date == SelectedDate.Date).FirstOrDefault();
+        if (selectedDate != null) 
+        {
+            selectedDate.IsCurrentDate = true;
+            _tempDate = selectedDate.Date;
+        }
     }
+
+    #region Commands
+    public ICommand CurrentDateCommand => new Command<CalendarModel>((currentDate) =>
+    {
+        _tempDate = currentDate.Date;
+        SelectedDate = currentDate.Date;
+        Dates.ToList().ForEach(f => f.IsCurrentDate = false);
+        currentDate.IsCurrentDate = true; 
+    });
+
+    public ICommand NextMonthCommand => new Command(() =>
+    {
+        _tempDate = _tempDate.AddMonths(1);
+        BindDates(_tempDate);
+    });
+
+    public ICommand PreviousMonthCommand => new Command(() =>
+    {
+        _tempDate = _tempDate.AddMonths(-1);
+        BindDates(_tempDate);
+    });
+
+    #endregion
 }
